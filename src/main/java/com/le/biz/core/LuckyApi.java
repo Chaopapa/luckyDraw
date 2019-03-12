7d2491508d5f77acf7f7b9df2d4fd02c91f4c317
lz0929 <897011520@qq.com>
2019-03-12 11:25:44 +0800

    修改抽奖规则

--- src/main/java/com/le/biz/core/LuckyApi.java
+++ src/main/java/com/le/biz/core/LuckyApi.java
@@ -1,5 +1,6 @@
 package com.le.biz.core;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.map.MapUtil;
@@ -7,15 +8,19 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.le.base.entity.LuckyRoster;
 import com.le.base.entity.LuckyRule;
 import com.le.base.entity.LuckyUser;
 import com.le.base.entity.dto.AisheliOrderDto;
+import com.le.base.entity.enums.RosterTypeEnum;
+import com.le.base.service.ILuckyRosterService;
 import com.le.base.service.ILuckyRuleService;
 import com.le.base.service.ILuckyUserService;
 import com.le.biz.TokenValidatorHandler;
 import com.le.core.properties.LongeProperties;
 import com.le.core.rest.R;
 import com.le.core.rest.RCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +36,7 @@ import java.util.stream.Collectors;
  * @Date 2019/3/6 16:49
  * @Version V1.0
  **/
+@Slf4j
 @RestController
 @RequestMapping("/templates/admin/biz/lucky")
 public class LuckyApi {
@@ -40,6 +46,9 @@ public class LuckyApi {
 
     @Autowired
     private ILuckyUserService luckyUserService;
+
+    @Autowired
+    private ILuckyRosterService luckyRosterService;
 
     @Autowired
     private LongeProperties longeProperties;
@@ -60,10 +69,11 @@ public class LuckyApi {
             paramMap.put("limitMinPrice", rule.getLimitMinPrice().toString());
             paramMap.put("limitMaxPrice", rule.getLimitMaxPrice().toString());
         }
+
+        List<String> nos = new ArrayList<>();
+        List<String> phones = new ArrayList<>();
         if(!(Boolean.TRUE.equals(rule.getLimitNo()) && Boolean.TRUE.equals(rule.getLimitNo()))){
             List<LuckyUser> luckyUsers = luckyUserService.getByRuleId(ruleId);
-            List<String> nos = new ArrayList<>();
-            List<String> phones = new ArrayList<>();
             if(CollectionUtil.isNotEmpty(luckyUsers)){
                 luckyUsers.stream().forEach(luckyUser -> {
                     if(Boolean.FALSE.equals(rule.getLimitNo())){
@@ -74,30 +84,76 @@ public class LuckyApi {
                     }
                 });
             }
-            if(CollectionUtil.isNotEmpty(nos)){
-                paramMap.put("luckyNoArr", nos);
+        }
+
+        List<AisheliOrderDto> orders = new ArrayList<>();
+        String no = null;
+
+        List<LuckyRoster> rosters = null;
+        if(Boolean.TRUE.equals(rule.getHaveMenu()) && Boolean.TRUE.equals(rule.getHaveBlacklist())){
+            rosters = luckyRosterService.findByRuleId(ruleId, null);
+        }else if(Boolean.TRUE.equals(rule.getHaveMenu())){
+            rosters = luckyRosterService.findByRuleId(ruleId, RosterTypeEnum.Menu);
+        }else if(Boolean.TRUE.equals(rule.getHaveBlacklist())){
+            rosters = luckyRosterService.findByRuleId(ruleId, RosterTypeEnum.Blacklist);
+        }
+
+        if(CollectionUtil.isNotEmpty(rosters)){
+            for (LuckyRoster roster : rosters) {
+                if(RosterTypeEnum.Menu.equals(roster.getType())) {
+                    AisheliOrderDto dto = new AisheliOrderDto();
+                    BeanUtil.copyProperties(roster, dto);
+                    orders.add(dto);
+                }else if(RosterTypeEnum.Blacklist.equals(roster.getType())){
+                    phones.add(roster.getPhone());
+                }
             }
-            if(CollectionUtil.isNotEmpty(phones)){
-                paramMap.put("luckyPhoneArr", phones);
+
+            if(CollectionUtil.isNotEmpty(orders)){
+                for (AisheliOrderDto order : orders) {
+                    if(nos.contains(order.getO())){
+                        continue;
+                    }
+                    return new R().putData("no", order.getO());
+                }
             }
         }
+
+        if(CollectionUtil.isNotEmpty(nos)){
+            paramMap.put("luckyNoArr", nos);
+        }
+
+        if(CollectionUtil.isNotEmpty(phones)){
+            paramMap.put("luckyPhoneArr", phones);
+        }
+
         Long beginTime = new Date().getTime();
         String orderStr = HttpUtil.post(longeProperties.getOrderUrl(), paramMap);
+
         Long totalConsumedTime = new Date().getTime() - beginTime;
-        System.out.println("订单接口耗时：" + totalConsumedTime);
+        log.info("订单接口耗时：" + totalConsumedTime);
         if (StrUtil.isNotBlank(orderStr)) {
             R r = JSON.parseObject(orderStr, R.class);
             if (RCode.success.getValue().equals(r.getRespCode())) {
                 Map<String, Object> data = r.getData();
                 String order = MapUtil.getStr(data, "orders");
-                List<AisheliOrderDto> orders = JSON.parseArray(order, AisheliOrderDto.class);
-                String no = luckyUserService.doDrawAndSaveUser(orders, ruleId);
-                Long lastTime = new Date().getTime() - interfaceTime;
-                System.out.println("接口总耗时：" + lastTime);
-                return new R().putData("no", no);
+                orders = JSON.parseArray(order, AisheliOrderDto.class);
+            }else if("0001".equals(r.getRespCode())){
+                return R.error("缺少抽奖时间参数");
+            }else if(RCode.empty.getValue().equals(r.getRespCode())){
+                return R.error("无单号可抽奖");
             }
         }
-        return R.error();
+        if(CollectionUtil.isNotEmpty(orders)){
+            no = luckyUserService.doDrawAndSaveUser(orders, ruleId);
+        }
+
+        if(StrUtil.isNotBlank(no)){
+            Long lastTime = new Date().getTime() - interfaceTime;
+            log.info("接口总耗时：" + lastTime);
+            return new R().putData("no", no);
+        }
+        return R.empty();
     }
 
 }
